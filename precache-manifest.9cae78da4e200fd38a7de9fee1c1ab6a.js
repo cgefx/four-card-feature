46bf70dde587c135a3baeb0a758a518d1802cd69
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-05-13 11:39:00 -0700

    Updates

---
+++ precache-manifest.9cae78da4e200fd38a7de9fee1c1ab6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fc5b1d5f4da2357f215b6d081ecefe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa79902b65ce209d8cc",
+    "url": "/static/css/main.9dbabcf9.chunk.css"
+  },
+  {
+    "revision": "150be8118e4a158f9b52",
+    "url": "/static/js/2.e8910bd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8910bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa79902b65ce209d8cc",
+    "url": "/static/js/main.02cfd69c.chunk.js"
+  },
+  {
+    "revision": "1f7011227f8426e8c4e2",
+    "url": "/static/js/runtime-main.c10bb941.js"
+  },
+  {
+    "revision": "8e4a5574d019096b62a948e62e920308",
+    "url": "/static/media/icon-calculator.8e4a5574.svg"
+  },
+  {
+    "revision": "ae2dc13dd8be5d72dc6ea67236f8729c",
+    "url": "/static/media/icon-karma.ae2dc13d.svg"
+  },
+  {
+    "revision": "8f7824b371e618577b20ba3775d9cfb4",
+    "url": "/static/media/icon-supervisor.8f7824b3.svg"
+  },
+  {
+    "revision": "8a1a6a173b407f9e45ba99fb7f25f0db",
+    "url": "/static/media/icon-team-builder.8a1a6a17.svg"
+  }
+]);
