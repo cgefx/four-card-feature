0338117a894f91b39de24f2b7193e999cb88f1df
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-05-12 19:49:01 -0700

    add styles

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ import './styles/styles.scss';
 
 function App() {
 	return (
-		<>
-			<div className='row'>
+		<div className='container'>
+			<div className='column flex-center'>
 				<header className='header'>
 					<h1 className='header__text'>
 						Reliable, efficient delivery
-						<br />
-						<span className='header__text--bold'>Powered by Technology</span>
+						<div className='header__text--bold'>Powered by Technology</div>
 					</h1>
-					<h4 className='header__sub'>
+					<h4 className='header__text--sub'>
 						Our Artificial Intelligence powered tools use millions of project data points
 						to ensure that your project is successful
 					</h4>
@@ -24,62 +23,73 @@ function App() {
 			</div>
 
 			<main>
-				<section className='row'>
-					<div className='column'>
-						<div className='card'>
-							<div className='card__title'>
-								<h2>Supervisor Monitors</h2>
+				<div className='section'>
+					<div className='row'>
+						{/* Column 1 */}
+						<div className='column flex-center'>
+							<div className='card column'>
+								<div className='card__title'>
+									<h2>Supervisor</h2>
+								</div>
+								<div className='card__text'>
+									<p className='paragraph'>
+										Monitors activity to identify project roadblocks{' '}
+									</p>
+								</div>
+								<div className='card__icon'>
+									<img src={supervisor} alt='' />
+								</div>
 							</div>
-							<div className='card__text'>
-								<p>activity to identify project roadblocks </p>
+						</div>
+						{/* Column 2 */}
+						<div className='column flex-center'>
+							<div className='card column'>
+								<div className='card__title'>
+									<h2>Team Builder</h2>
+								</div>
+								<div className='card__text'>
+									<p className='paragraph'>
+										Scans our talent network to create the optimal team for your project
+									</p>
+								</div>
+								<div className='card__icon'>
+									<img src={teamBuilder} alt='' />
+								</div>
 							</div>
-							<div className='card__icon'>
-								<img src={supervisor} alt='' />
+							<div className='card column'>
+								<div className='card__title'>
+									<h2>Karma</h2>
+								</div>
+								<div className='card__text'>
+									<p className='paragraph'>
+										Regularly evaluates our talent to ensure quality
+									</p>
+								</div>
+								<div className='card__icon'>
+									<img src={karma} alt='' />
+								</div>
+							</div>
+						</div>
+						{/* Column 3 */}
+						<div className='column flex-center'>
+							<div className='card column'>
+								<div className='card__title'>
+									<h2>Calculator</h2>
+								</div>
+								<div className='card__text'>
+									<p className='paragraph'>
+										Uses data from past projects to provide better delivery estimates
+									</p>
+								</div>
+								<div className='card__icon'>
+									<img src={calculator} alt='' />
+								</div>
 							</div>
 						</div>
 					</div>
-					<div className='column'>
-						<div className='card'>
-							<div className='card__title'>
-								<h2>Team Builder</h2>
-							</div>
-							<div className='card__text'>
-								<p>
-									Scans our talent network to create the optimal team for your project
-								</p>
-							</div>
-							<div className='card__icon'>
-								<img src={teamBuilder} alt='' />
-							</div>
-						</div>
-						<div className='card'>
-							<div className='card__title'>
-								<h2>Karma</h2>
-							</div>
-							<div className='card__text'>
-								<p>Regularly evaluates our talent to ensure quality</p>
-							</div>
-							<div className='card__icon'>
-								<img src={karma} alt='' />
-							</div>
-						</div>
-					</div>
-					<div className='column'>
-						<div className='card'>
-							<div className='card__title'>
-								<h1>Calculator</h1>
-							</div>
-							<div className='card__text'>
-								<p> Uses data from past projects to provide better delivery estimates</p>
-							</div>
-							<div className='card__icon'>
-								<img src={calculator} alt='' />
-							</div>
-						</div>
-					</div>
-				</section>
+				</div>
 			</main>
-		</>
+		</div>
 	);
 }
 
